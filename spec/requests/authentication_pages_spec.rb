89e489ad9d21dcf6c1301bd0f0a5d7273f2f7436
Chellberg <corinchellberg@gmail.com>
2013-09-21 16:14:32 -0400

    Working settings page functionality when user signed in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -20,6 +20,7 @@ describe "Authentication" do
       it { should have_selector('title', text: 'Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
       it { should have_error_message('Invalid') }
+    
       
       describe "after visiting another page" do
         before { click_link "Home" }
@@ -34,6 +35,7 @@ describe "Authentication" do
       
       it { should have_selector('title', text: user.name) }
       it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
       
