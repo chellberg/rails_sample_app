e2e78af137b0b0b35bf86e7c2e68ea0097a37da2
Chellberg <corinchellberg@gmail.com>
2013-09-23 19:00:19 -0400

    additional friendly forwarding test

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -52,23 +52,36 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
       
-      describe "when attempting to visit a protected page" do #FriendlyForwarding
+      describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
-          fill_in "Email", with: user.email
+          fill_in "Email",    with: user.email
           fill_in "Password", with: user.password
           click_button "Sign in"
         end
 
         describe "after signing in" do
-          it "should render the desired protected page" do           
+
+          it "should render the desired protected page" do
             page.should have_selector('title', text: 'Edit user')
           end
 
-        end
-      
-      end
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
 
+            it "should render the default (profile) page" do
+              page.should have_selector('title', text: user.name)
+            end
+          end
+        end
+      end
+    
       describe "as a non-admin user" do
         let(:user) { FactoryGirl.create(:user) }
         let(:non_admin) { FactoryGirl.create(:user) }
